369b70c09dd2e539edc1ae9b1efe39fc4c2ac476
Skeja <jasovicluka1@gmail.com>
2025-05-11 18:43:22 +0200

    tracked fasta and sh and pushed with git extension due to large files

--- bioinformatics/chapter3.sh
+++ bioinformatics/chapter3.sh
@@ -1,31 +1,3 @@
-#! /bin/bash
-
-## chapter 3: Declare Variables and read inputs
-#define variables
-
-city="Belgrade"
-echo $city
-echo "$city"
-
-
-echo ${city/grade/_grade}
-echo ${city:0:3}
-echo ${#city}
-
-echo ${city:-'Default value'}
-
-fruit="apple"
-echo ${#fruit}
-
-
-echo ${city:-"abc"}
-
-# default value
-
-
-# reading user input
-
-read -p "which is your fav. fruit? " fruit
-echo $fruit
-
-# first part after -p is user input and it is stored in the variable "fruit", after that next line prints the stored variable value
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a467175ff6701d3ee38c3cbc929188f390c017b2fc2346cdf2a985222835419
+size 492
---
+++ bioinformatics/gpx4_bulk_fetch.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e562a3b2cc9095b5d1dd1fcdbaf8b42efb82568da857bfd5c990046bab7f8ab6
+size 369
